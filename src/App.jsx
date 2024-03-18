051affe874dab139abb1a7f1e5471236a62d9cb7
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-03-19 01:26:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sing from "./assets/components/Sing/Sing";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <Sing></Sing>
+      </div>
+    </>
+  );
+}
+
+export default App;
